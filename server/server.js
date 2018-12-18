64da8de7adb14bd4a946b410fa5a66820444bae6
Silvia <silviquir6@gmail.com>
2018-12-18 07:32:36 -0500

    se cambia access control 3

--- server/server.js
+++ server/server.js
@@ -12,8 +12,7 @@ const app = express()
 // CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, token, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
 });
