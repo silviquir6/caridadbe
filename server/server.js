e9aef4a4d2545dd186a806e8918b666da89f6d0b
Silvia <silviquir6@gmail.com>
2018-12-18 07:46:34 -0500

    se agrega cors

--- server/server.js
+++ server/server.js
@@ -9,12 +9,18 @@ const path = require('path');
 
 const app = express()
 
+//CORS
+var cors = require('cors')
+
+app.use(cors());
+
+
 // CORS
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+}); */
 
 
 const bodyParser = require('body-parser')
