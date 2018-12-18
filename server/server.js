fceb2d0e99420b3ed15bd186713580f6fd1f5649
Silvia <silviquir6@gmail.com>
2018-12-18 07:07:29 -0500

    se cambia access control

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 // CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://caridadhumana.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
